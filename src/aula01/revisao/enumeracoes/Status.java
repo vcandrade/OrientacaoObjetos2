3713cbeb625b609c4253e9c76af80345b340497d
Vinicius Camargo Andrade <vcandrade@utfpr.edu.br>
2026-03-13 00:19:59 -0300

    added class 01 and 02

---
+++ src/aula01/revisao/enumeracoes/Status.java
@@ -0,0 +1,9 @@
+package aula01.revisao.enumeracoes;
+
+public enum Status {
+
+	AGUARDANDO_PAGAMENTO,
+	PROCESSANDO,
+	ENVIADO,
+	ENTREGUE;
+}
---
+++ src/aula01/revisao/padroesnomenclatura/Teste.java
@@ -0,0 +1,13 @@
+package aula01.revisao.padroesnomenclatura;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		
+		ContaCorrente contaCorrente = new ContaCorrente();
+		
+		Matriz matriz = new Matriz();
+	}
+}
